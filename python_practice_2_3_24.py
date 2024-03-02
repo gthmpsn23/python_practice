d1f5751827d56b30d72c3b6e97fd683cf08e4d47
“@“gthmpsn23” <gareththompson207@gmail.com>
2024-03-02 09:25:59 +0000

    Strings end with?

--- python_practice_2_3_24.py
+++ python_practice_2_3_24.py
@@ -1,17 +1,43 @@
 # Function that calculates the points accumilated by football team over a season 
 
-def points(games):
-    points = 0
-    for game in games:
-        x , y = game.split(':')
-        x = int(x)
-        y = int(y)
-        if x > y:
-            points += 3
-        elif x < y:
-            continue
-        else:
-            points += 1
-    return points
+# def points(games):
+#     points = 0
+#     for game in games:
+#         x , y = game.split(':')
+#         x = int(x)
+#         y = int(y)
+#         if x > y:
+#             points += 3
+#         elif x < y:
+#             continue
+#         else:
+#             points += 1
+#     return points
 
-print(points(['1:0','2:0','3:0','4:0','2:1','3:1','4:1','3:2','4:2','4:3']))
+# print(points(['1:0','2:0','3:0','4:0','2:1','3:1','4:1','3:2','4:2','4:3']))
+
+# String ends with?
+
+def solution(text, ending):
+    last_characters = text[-len(ending):]
+    # ending_last = ending[-1:]
+    if last_characters == ending:
+        return True
+    else:
+        return False
+
+print(solution("sumo", "omo"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
